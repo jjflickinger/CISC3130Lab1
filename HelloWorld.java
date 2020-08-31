563a64bc613bdfa131b2899750db2aba68b0eed7
Jacob Flickinger <69496818+jjflickinger@users.noreply.github.com>
2020-08-30 22:22:16 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,6 @@
-//This program prints out the phrase "Hello, world"
+//Simple program that prints a single phrase and a newline
+//No input
+//Output "Hello, world"
 
 public class HelloWorld{
   public static void main(String[] args) {
